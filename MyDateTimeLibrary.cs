34610019ba0619e78df4ff9776165e10e76a33de
Marsel Ismakaev <imkcamel@gmail.com>
2023-02-17 20:51:16 +0600

    Add project files.

---
+++ MyDateTimeLibrary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MyDateTimeLibrary
+{
+    [Serializable]
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class DateTimeFunctions
+    {
+        public DateTimeFunctions(){}
+
+        public DateTime UnixTimeToDateTime(int unixTimeInSeconds)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(unixTimeInSeconds).UtcDateTime;
+        }
+
+        public long DateTimeUtcToUnixTime(DateTime utcDateTime)
+        {
+            return new DateTimeOffset(utcDateTime, TimeSpan.Zero).ToUnixTimeSeconds();
+        }
+
+        public DateTime UtcToLocal(DateTime utcDateTime)
+        {
+            return utcDateTime.ToLocalTime();
+        }
+
+        public DateTime LocalToUtc(DateTime localDateTime)
+        {
+            return localDateTime.ToUniversalTime();
+        }
+
+        public DateTime TZSpecificDateTimeToUTC(DateTime sourceDateTime, string sourceTimeZoneId)
+        {
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById(sourceTimeZoneId);
+            return TimeZoneInfo.ConvertTimeToUtc(sourceDateTime, tzi);
+        }
+
+        public DateTime UTCToTZSpecificDateTime(DateTime utcDateTime, string destinationTimeZoneId)
+        {
+            var tzi = TimeZoneInfo.FindSystemTimeZoneById(destinationTimeZoneId);
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, tzi);
+        }
+    }
+}
